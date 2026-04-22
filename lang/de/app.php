c44edb054aba7554f929f013566ecb7b047cbd22
Christopher Falkner <17108146+Dhalion@users.noreply.github.com>
2026-04-22 21:52:49 +0200

    feat: add translations and opengraph link preview support

---
+++ lang/de/app.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'title' => 'Tipinuss',
+    'description' => 'Online Waschnusswetten - Tippe deine Wetten, verdiene Waschnüsse. Die moderne Wettplatform für Enthusiasten.',
+    'navigation' => [
+        'home' => 'Startseite',
+        'bets' => [
+            'list' => 'Wetten',
+            'create' => 'Wette erstellen',
+        ],
+        'account' => 'Konto',
+        'login' => 'Anmelden',
+        'register' => 'Registrieren',
+        'logout' => 'Abmelden',
+    ],
+];
---
+++ lang/en/app.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'title' => 'Tipinuss',
+    'description' => 'Online Walnut Bets - Place your bets, earn walnuts. The modern betting platform for enthusiasts.',
+    'navigation' => [
+        'home' => 'Home',
+        'bets' => [
+            'list' => 'Bets',
+            'create' => 'Create Bet',
+        ],
+        'account' => 'Account',
+        'login' => 'Login',
+        'register' => 'Register',
+        'logout' => 'Logout',
+    ],
+];
