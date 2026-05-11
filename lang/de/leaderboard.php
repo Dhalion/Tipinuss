43ecf25d75e14e01b00880832f4dd3ca3d8fe788
Christopher Falkner <17108146+Dhalion@users.noreply.github.com>
2026-05-11 21:23:46 +0200

    feat: add admin and leaderboard translation files

---
+++ lang/de/leaderboard.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title' => 'Top Bettors',
+    'description' => 'Die erfolgreichsten Wettenden',
+    'table' => [
+        'rank' => '#',
+        'bettor' => 'Bettor',
+        'bets' => 'Wetten',
+        'winnings' => 'Gewinne',
+    ],
+    'empty' => 'Noch keine Bettors vorhanden',
+];
